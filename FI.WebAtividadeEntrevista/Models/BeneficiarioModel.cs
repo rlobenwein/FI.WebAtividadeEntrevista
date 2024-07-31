bcd1960e263ed5ae7417e6242923c43f7519c3c8
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2024-07-31 14:32:24 -0300

    Criado modelo Beneficiario

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FI.WebAtividadeEntrevista.Models
+{
+    /// <summary>
+    /// Classe de Modelo de Beneficiário
+    /// </summary>
+    public class BeneficiarioModel
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        [MaxLength(11)]
+        public string Cpf { get; set; }
+
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Required]
+        public long IdCliente { get; set; }
+
+    }
+}
