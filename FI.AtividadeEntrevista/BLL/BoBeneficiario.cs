4228b0174e5e6e655b369a438e8f722916a85a8e
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2024-07-31 16:39:29 -0300

    Criação da classe de operação de negócios BoBeneficiarios.cs

---
+++ FI.AtividadeEntrevista/BLL/BoBeneficiario.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using FI.AtividadeEntrevista.DAL;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoBeneficiario
+    {
+        /// <summary>
+        /// Inclui um novo beneficiario
+        /// </summary>
+        /// <param name="beneficiario">Objeto de beneficiario</param>
+        public long Incluir(DML.Beneficiario beneficiario)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            return ben.Incluir(beneficiario);
+        }
+
+        /// <summary>
+        /// Altera um beneficiario
+        /// </summary>
+        /// <param name="beneficiario">Objeto de beneficiario</param>
+        public void Alterar(DML.Beneficiario beneficiario)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            ben.Alterar(beneficiario);
+        }
+
+        /// <summary>
+        /// Consulta o beneficiario pelo id
+        /// </summary>
+        /// <param name="id">id do beneficiario</param>
+        /// <returns></returns>
+        public DML.Beneficiario Consultar(long id)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            return ben.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o beneficiario pelo id
+        /// </summary>
+        /// <param name="id">id do beneficiario</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            ben.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os beneficiarios
+        /// </summary>
+        public List<DML.Beneficiario> Listar()
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            return ben.Listar();
+        }
+
+        /// <summary>
+        /// Lista os beneficiarios
+        /// </summary>
+        public List<DML.Beneficiario> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            return ben.Pesquisa(iniciarEm, quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// VerificaExistencia
+        /// </summary>
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string CPF)
+        {
+            DaoBeneficiario ben = new DaoBeneficiario();
+            return ben.VerificarExistencia(CPF);
+        }
+
+    }
+}
